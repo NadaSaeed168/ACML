2b23cadfd6559ddb7d6e2d1642475ec69db038c2
unknown <NadaSaeed168>
2018-12-03 21:12:31 +0200

    third commit

--- main.go
+++ main.go
@@ -1,50 +1,152 @@
 package main
 import (
-	//"html/template"
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	//"log"
+    "log"
+    "net/http"
+    "text/template"
 	"io"
-	"net/http"
-	//"fmt"
-	//"time"
+	"fmt"
+    _ "github.com/go-sql-driver/mysql"
 )
-type PageVariables struct {
-	Date string
-	Time string
+type Notes struct {
+    Id    int
+    Description  string
+    Location string
+}
+func dbConn() (db *sql.DB) {
+    dbDriver := "mysql"
+    dbUser := "root"
+    dbPass := "password"
+    dbName := "notes"
+    db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+    if err != nil {
+        panic(err.Error())
+    }
+    return db
 }
 func main() {
-http.HandleFunc("/", index)
-http.ListenAndServe(":80", nil)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/show", Show)
+    http.HandleFunc("/edit", Edit)
+    http.HandleFunc("/insert", Insert)
+    http.HandleFunc("/update", Update)
+    http.HandleFunc("/delete", Delete)
+	http.ListenAndServe(":8080", nil)
 }
+var tmpl = template.Must(template.ParseGlob("form/*"))
 func index(w http.ResponseWriter, r *http.Request) {
 io.WriteString(w, "hello from a docker container")
-io.WriteString(w, "Go MYSQL Tutorial")
-db, err := sql.Open("mysql", "root:password@tcp(:80)/database")
-if err != nil{
+db := dbConn()
+fmt.Println("MYSQL Tutorial")
+//insert, err := db.Prepare("INSERT INTO notes VALUES(?,?,?)")
+selDB, err := db.Query("SELECT * FROM notes")
+if err != nil {
 	panic(err.Error())
 }
+emp := Notes{}
+res := []Notes{}
+for selDB.Next() {
+	var ID int
+	var description0, location string
+	err = selDB.Scan(&ID, &description0, &location)
+	if err != nil {
+		panic(err.Error())
+	}
+	emp.Id = ID
+	emp.Description = description0
+	emp.Location = location
+	res = append(res, emp)
+}
+tmpl.ExecuteTemplate(w, "Index", res)
 defer db.Close()
-io.WriteString(w,"MYSQL Tutorial")
-insert, err := db.Prepare("INSERT INTO notes VALUES('5ara el senin','GUC')")
-if err != nil{
-	panic(err.Error())
-	io.WriteString(w,"MYSQL")
+fmt.Println("Successfully inserted into user tables")
 }
-io.WriteString(w,"MYSQL Table")
-defer insert.Close()
-io.WriteString(w,"Successfully inserted into user tables")
-// now := time.Now() // find the time right now
-// HomePageVars := PageVariables{ //store the date and time in a struct
-// Date: now.Format("02-01-2006"),
-// Time: now.Format("15:04:05"),
-// }
-// t, err := template.ParseFiles("homepage.html") //parse the html file homepage.html
-// if err != nil { // if there is an error
-// log.Print("template parsing error: ", err) // log it
-// }
-// err = t.Execute(w, HomePageVars) //execute the template and pass it the HomePageVars struct to fill in the gaps
-// if err != nil { // if there is an error
-// log.Print("template executing error: ", err) //log it
-// }
+func Show(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    nId := r.URL.Query().Get("ID")
+    selDB, err := db.Query("SELECT * FROM notes WHERE ID=?", nId)
+    if err != nil {
+        panic(err.Error())
+    }
+    emp := Notes{}
+    for selDB.Next() {
+        var ID int
+        var description0, location string
+        err = selDB.Scan(&ID, &description0, &location)
+        if err != nil {
+            panic(err.Error())
+        }
+        emp.Id = ID
+        emp.Description = description0
+        emp.Location = location
+    }
+    tmpl.ExecuteTemplate(w, "Show", emp)
+    defer db.Close()
+}
+func Edit(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    nId := r.URL.Query().Get("ID")
+    selDB, err := db.Query("SELECT * FROM notes WHERE ID=?", nId)
+    if err != nil {
+        panic(err.Error())
+    }
+    emp := Notes{}
+    for selDB.Next() {
+        var ID int
+        var description0, location string
+        err = selDB.Scan(&ID, &description0, &location)
+        if err != nil {
+            panic(err.Error())
+        }
+        emp.Id = ID
+        emp.Description = description0
+        emp.Location = location
+    }
+    tmpl.ExecuteTemplate(w, "Edit", emp)
+    defer db.Close()
+}
+func Insert(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    if r.Method == "POST" {
+        description0 := r.FormValue("description0")
+        location := r.FormValue("location")
+        insForm, err := db.Prepare("INSERT INTO notes(description0, location) VALUES(?,?)")
+        if err != nil {
+            panic(err.Error())
+        }
+        insForm.Exec(description0, location)
+        log.Println("INSERT: Description: " + description0 + " | location: " + location)
+    }
+    defer db.Close()
+    http.Redirect(w, r, "/", 301)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    if r.Method == "POST" {
+        description0 := r.FormValue("description0")
+        location := r.FormValue("location")
+        ID := r.FormValue("Id")
+        insForm, err := db.Prepare("UPDATE notes SET description0=?, location=?")
+        if err != nil {
+            panic(err.Error())
+        }
+        insForm.Exec(description0, location, ID)
+        log.Println("UPDATE: description: " + description0 + " | location: " + location)
+    }
+    defer db.Close()
+    http.Redirect(w, r, "/", 301)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+    db := dbConn()
+    emp := r.URL.Query().Get("ID")
+    delForm, err := db.Prepare("DELETE FROM Employee")
+    if err != nil {
+        panic(err.Error())
+    }
+    delForm.Exec(emp)
+    log.Println("DELETE")
+    defer db.Close()
+    http.Redirect(w, r, "/", 301)
 }
